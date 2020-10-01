c9c6c68a011063e947d9586680024f89428a2801
Nick Charbonneau <nick@sigmaiq.com>
2020-09-30 19:54:27 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import CONFIG from './config'
 
 import indexRouter from './routes/index';
 
 const app = express();
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
